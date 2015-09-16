0739843ad5fdb0126491f32ffc728b62e71b1c8c
blaedj <blaedj@gmail.com>
2015-09-16 08:43:24 -0500

    Basic addition works, if you are careful

--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -10,16 +10,80 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var calcDisplay: UILabel!
+   
+    var appendToDisplay = false
+    var operandStack = Array<Double>()
+    var operatorStack = Array<Operator>()
+   
+    @IBAction func digitInput(sender: UIButton) {
+        let digit = sender.currentTitle!
+        updateDisplay(digit)
+        appendToDisplay = true
+    }
+    
+    
+    @IBAction func enterPressed() {
+        appendToDisplay = false
+        operandStack.append(displayValue)
+        print(operandStack)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func calculate() -> Double? {
+        appendToDisplay = false
+        if let lastOperator = operatorStack.popLast() {
+            let val = lastOperator.perform(operandStack.popLast()!, secondOperand: operandStack.popLast()!)
+            return val
+        }
+        return 0.0
     }
 
+    @IBAction func addPressed() {
+        operatorStack.append(Adder())
+        let result = calculate()!
+        updateDisplay("\(result)")
+        operandStack.append(result)
+    }
 
+    func updateDisplay(message: String){
+        if appendToDisplay {
+            calcDisplay.text = calcDisplay.text! + message
+        } else {
+            calcDisplay.text = message
+        }
+    }
+
+    var displayValue: Double {
+        get {
+            let value = NSNumberFormatter().numberFromString(calcDisplay.text!)!.doubleValue
+            return value
+        }
+        set {
+            calcDisplay.text = "\(newValue)"
+        }
+    }
 }
 
+
+
+//-----------should move to a new file---------------------------------
+
+class Operator {
+    func perform(firstOperand: Double, secondOperand: Double) -> Double {
+        return firstOperand
+    }
+}
+
+
+class Adder: Operator {
+
+    override func perform(firstOperand: Double, secondOperand: Double) -> Double {
+        return firstOperand + secondOperand
+    }
+}
+
+class Subtracter: Operator {
+    override func perform(firstOperand: Double, secondOperand: Double) -> Double {
+        return firstOperand - secondOperand
+    }
+}
